a44c0c7df469f2e76148cd5e883978a9a8a161f1
hj <hj@toss.im>
2020-03-12 22:39:21 +0900

    * url_for refactoring 완료

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -26,7 +26,7 @@ def send_reset_email(user):
                   sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)} 
+{url_for('users.reset_token', token=token, _external=True)} 
 
 If your did not make this request then simply ignore this email and no change will be made.
 '''
